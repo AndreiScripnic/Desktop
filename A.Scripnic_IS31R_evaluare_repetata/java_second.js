fd5c59d22ea6a48f076ea96201ccf9e3b6aa0749
Scripnic Andrei <sablezubiiork@gmail.com>
2021-01-19 16:05:16 +0200

    insarcinarea 2

---
+++ A.Scripnic_IS31R_evaluare_repetata/java_second.js
@@ -0,0 +1,10 @@
+//перебора элементов
+let arr = [0,1,2,3,4,5,6,7,8,9];
+for (let i = 0; i < arr.length; i++) {
+    alert( arr[i] );
+  }
+
+  //let culoare = ["rosu", "verde", "negru"];
+//for (let culoare of culorile) {
+  //alert( culoare );
+//}
