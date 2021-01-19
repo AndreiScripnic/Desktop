2ea90982150c434640949078aac9c3ccc2d5f146
Scripnic Andrei <sablezubiiork@gmail.com>
2021-01-19 15:44:20 +0200

    insarcinarea 1

--- A.Scripnic_IS31R_evaluare_repetata/java_first.js
+++ A.Scripnic_IS31R_evaluare_repetata/java_first.js
@@ -1,6 +1,40 @@
-console.log("Hello word from js")
-document.addEventListener("DOMContentLoaded",)
-console.clear();
-let x = "2";
-let y = "5a";
-console.log(x + y);
+let arr = [0,1,2,3,4,5,6,7,8,9];
+//let n = 5;
+for (let index = 0; index < arr.length; index++) {
+    switch (arr[index]) {
+        case 0:
+            alert('Ati introdus zero');
+            break;
+        case 1:
+            alert('Ati introdus unu');
+            break;
+        case 2:
+            alert('Ati introdus doi');
+            break;
+            case 3:
+            alert('Ati introdus trei');
+            break;
+            case 4:
+            alert('Ati introdus patru');
+            break;
+            case 5:
+            alert('Ati introdus cinci');
+            break;
+            case 6:
+            alert('Ati introdus sase');
+            break;
+            case 7:
+            alert('Ati introdus sapte');
+            break;
+            case 8:
+            alert('Ati introdus opt');
+            break;
+            case 9:
+            alert('Ati introdus noua');
+            break;
+        default:
+            break;
+    }
+    
+}
+
